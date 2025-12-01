20931fad703806736d9b8ba29f930e38908af668
Pranitha <pranithareddy160805@gmail.com>
2025-12-02 03:28:39 +0530

    Modified tests

--- flight-service/src/test/java/com/flightapp/demo/controller/SeatControllerTest.java
+++ flight-service/src/test/java/com/flightapp/demo/controller/SeatControllerTest.java
@@ -7,8 +7,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
