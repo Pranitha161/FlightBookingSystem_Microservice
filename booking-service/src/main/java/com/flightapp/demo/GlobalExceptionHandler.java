1082505f331958e1ddf3ab06e756486cb2cc2819
Pranitha <pranithareddy160805@gmail.com>
2025-12-05 14:03:14 +0530

    Added gloabal exception handler

---
+++ booking-service/src/main/java/com/flightapp/demo/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.flightapp.demo;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<List<String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(error -> error.getDefaultMessage())
+				.collect(Collectors.toList());
+		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+	}
+}
